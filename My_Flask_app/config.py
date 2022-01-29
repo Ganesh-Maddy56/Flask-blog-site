299118ada5a8a7baf3bfe4a001ca580a2e02d4aa
Mohammedsharook <aydensharu@gmail.com>
2022-01-29 16:34:11 +0530

    updated errors

--- My_Flask_app/config.py
+++ My_Flask_app/config.py
@@ -1,6 +1,4 @@
-from os import environ
-
-SECRET_KEY = environ.get('SECRET_KEY')
+SECRET_KEY = '75e64d694b5c6602148044c51106738c'
 
 # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:shar@localhost/Information'
 SQLALCHEMY_DATABASE_URI = 'postgresql://iuzbecbpybtrvg:70ecee323a993a2e4ef23c9d67e3a33c13170882f8af650327e67f1fd0cea9e8@ec2-107-21-146-133.compute-1.amazonaws.com:5432/dem7s52dbopn3f'
