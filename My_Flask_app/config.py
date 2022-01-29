ffeb647ba86c4aee94a2cd7fcc8b8642ae75ec94
Mohammedsharook <aydensharu@gmail.com>
2022-01-29 16:02:21 +0530

    newappheroku

--- My_Flask_app/config.py
+++ My_Flask_app/config.py
@@ -2,8 +2,8 @@ from os import environ
 
 SECRET_KEY = environ.get('SECRET_KEY')
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:shar@localhost/Information'
-# SQLALCHEMY_DATABASE_URI = ' postgres://wizwvackguifwg:662e1eb83c54be438ed25af8074574099ba8718ff56f28368ec0888f018e33ea@ec2-18-213-179-70.compute-1.amazonaws.com:5432/d3mo6hccsm4tjs'
+# SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:shar@localhost/Information'
+SQLALCHEMY_DATABASE_URI = ' postgres://iuzbecbpybtrvg:70ecee323a993a2e4ef23c9d67e3a33c13170882f8af650327e67f1fd0cea9e8@ec2-107-21-146-133.compute-1.amazonaws.com:5432/dem7s52dbopn3f'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = False
 FLASK_ADMIN_SWATCH = 'Cosmo'
