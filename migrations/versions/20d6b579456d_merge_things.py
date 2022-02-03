0ef2930005d90c882c009081a6b02e9fc03aafef
Mohammedsharook <aydensharu@gmail.com>
2022-02-03 09:13:51 +0530

    add'

---
+++ migrations/versions/20d6b579456d_merge_things.py
@@ -0,0 +1,24 @@
+"""merge things
+
+Revision ID: 20d6b579456d
+Revises: 5d2bb32c6e04, 9f3555fcea34
+Create Date: 2022-02-02 12:33:13.933500
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '20d6b579456d'
+down_revision = ('5d2bb32c6e04', '9f3555fcea34')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
