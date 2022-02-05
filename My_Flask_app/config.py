2aabf38e63f29077baf28d13e30686e1337e13eb
Mohammedsharook <aydensharu@gmail.com>
2022-02-06 03:34:12 +0530

    heroku

--- My_Flask_app/config.py
+++ My_Flask_app/config.py
@@ -1,7 +1,7 @@
 SECRET_KEY = '75e64d694b5c6602148044c51106738c'
 
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:shar@localhost/Information'
-# SQLALCHEMY_DATABASE_URI = 'postgresql://iuzbecbpybtrvg:70ecee323a993a2e4ef23c9d67e3a33c13170882f8af650327e67f1fd0cea9e8@ec2-107-21-146-133.compute-1.amazonaws.com:5432/dem7s52dbopn3f'
+SQLALCHEMY_DATABASE_URI = 'postgresql://riikpexibzkgjj:a1e2495f191d04f8a556ed70c9e019fbc11df2ec4b48b160d8f888303acbdb25@ec2-54-157-15-228.compute-1.amazonaws.com:5432/dbkalo1rgu5esp'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = False
 
