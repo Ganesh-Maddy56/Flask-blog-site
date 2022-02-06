da8fbf53ee5076717207e2f80d9df5fc9e78f013
Mohammedsharook <aydensharu@gmail.com>
2022-02-07 01:00:20 +0530

    new

--- My_Flask_app/config.py
+++ My_Flask_app/config.py
@@ -1,7 +1,7 @@
 SECRET_KEY = '75e64d694b5c6602148044c51106738c'
 
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:shar@localhost/Information'
-SQLALCHEMY_DATABASE_URI = 'postgresql://riikpexibzkgjj:a1e2495f191d04f8a556ed70c9e019fbc11df2ec4b48b160d8f888303acbdb25@ec2-54-157-15-228.compute-1.amazonaws.com:5432/dbkalo1rgu5esp'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://riikpexibzkgjj:a1e2495f191d04f8a556ed70c9e019fbc11df2ec4b48b160d8f888303acbdb25@ec2-54-157-15-228.compute-1.amazonaws.com:5432/dbkalo1rgu5esp'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = False
 
