50dce9b2762159947b6c67974fdf06ea10cea5c7
Ganesh <ganeshmaddy354@gmail.com>
2022-02-08 13:47:19 +0530

    mysql client version change

--- My_Flask_app/config.py
+++ My_Flask_app/config.py
@@ -1,10 +1,6 @@
 SECRET_KEY = '75e64d694b5c6602148044c51106738c'
 
-SQLALCHEMY_DATABASE_URI = 'mysql://admin:uUF9oZ7og43tjH7nMjNg@database-1.cgm8gxm96tg4.us-west-2.rds.amazonaws.com/database-1'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:uUF9oZ7og43tjH7nMjNg@database-1.cgm8gxm96tg4.us-west-2.rds.amazonaws.com/database-1'
 # SQLALCHEMY_DATABASE_URI = 'postgresql://riikpexibzkgjj:a1e2495f191d04f8a556ed70c9e019fbc11df2ec4b48b160d8f888303acbdb25@ec2-54-157-15-228.compute-1.amazonaws.com:5432/dbkalo1rgu5esp'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = False
-
-
-
-   
